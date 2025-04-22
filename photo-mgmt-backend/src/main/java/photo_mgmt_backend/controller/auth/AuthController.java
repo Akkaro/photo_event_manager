2f2ccad2854104cb615468b820c375a81426c16a
Akkaro <akos.beno@gmail.com>
2025-04-22 21:47:59 +0300

    started to implement registration logic

--- photo-mgmt-backend/src/main/java/photo_mgmt_backend/controller/auth/AuthController.java
+++ photo-mgmt-backend/src/main/java/photo_mgmt_backend/controller/auth/AuthController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import photo_mgmt_backend.exception.model.ExceptionBody;
 import photo_mgmt_backend.model.dto.auth.LoginRequestDTO;
 import photo_mgmt_backend.model.dto.auth.LoginResponseDTO;
+import photo_mgmt_backend.model.dto.auth.RegisterRequestDTO;
+import photo_mgmt_backend.model.dto.auth.RegisterResponseDTO;
 
 @RequestMapping("/v1/auth")
 @Tag(name = "Authentication", description = "Operations for user authentication and login")
@@ -33,4 +35,17 @@ public interface AuthController {
     })
     @ResponseStatus(HttpStatus.OK)
     void login(@RequestBody @Valid LoginRequestDTO loginRequestDTO);
+
+    @PostMapping("/register")
+    @Operation(summary = "User registration", description = "Register user, save in db and return to login.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Registration successful",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = RegisterResponseDTO.class))),
+            @ApiResponse(responseCode = "401", description = "User already registered.",
+                    content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ExceptionBody.class)))
+    })
+    @ResponseStatus(HttpStatus.OK)
+    void register(@RequestBody @Valid RegisterRequestDTO registerRequestDTO);
 }
