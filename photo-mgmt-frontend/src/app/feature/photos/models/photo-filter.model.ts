49df53d84cf2cf23babdda621b27dffbff1c5f49
Akkaro <akos.beno@gmail.com>
2025-05-17 21:03:13 +0300

    photo filter error solved

--- photo-mgmt-frontend/src/app/feature/photos/models/photo-filter.model.ts
+++ photo-mgmt-frontend/src/app/feature/photos/models/photo-filter.model.ts
@@ -2,8 +2,8 @@ import { FilterRequest } from '../../../shared/models/filter-request-dto.model';
 
 
 export interface PhotoFilter extends FilterRequest {
-  albumId: string;
-  ownerId: string;
-  isEdited: boolean;
-  uploadedAt: string;
+  albumId?: string;
+  ownerId?: string;
+  isEdited?: boolean;
+  uploadedAt?: string;
 }
